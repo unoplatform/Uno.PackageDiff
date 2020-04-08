abf946d7499b4585281a28268440d3ef1e462909
David Oliver <david.oliver@nventive.com>
2020-04-07 22:08:41 -0400

    feat: Ignore changes to an ignored type
    If a type is in the Types of an ignore file, but is not actually removed, ignore all removed members on that type instead.
    
    In general ignoring individual members explicitly is preferable, but this option is useful in certain special cases, eg breaking changes involving large numbers of properties coming from generated code.

--- src/Uno.PackageDiff/ReportAnalyzer.cs
+++ src/Uno.PackageDiff/ReportAnalyzer.cs
@@ -9,11 +9,16 @@ namespace Uno.PackageDiff
 	{
 		public static bool IsDiffFailed(ComparisonResult results, IgnoreSet ignoreSet)
 		{
-			var failedTypes = results.InvalidTypes.Any(t => !ignoreSet.Types.Select(t2 => t2.FullName).Contains(t.ToSignature()));
-			var failedEvents = results.InvalidEvents.Any(e => !ignoreSet.Events.Select(t => t.FullName).Contains(e.ToSignature()));
-			var failedFields = results.InvalidFields.Any(f => !ignoreSet.Fields.Select(t => t.FullName).Contains(f.ToSignature()));
-			var failedMethods = results.InvalidMethods.Any(m => !ignoreSet.Methods.Select(t => t.FullName).Contains(m.ToSignature()));
-			var failedProperties = results.InvalidProperties.Any(p => !ignoreSet.Properties.Select(t => t.FullName).Contains(p.ToSignature()));
+			var ignoredTypes = ignoreSet.Types.Select(t2 => t2.FullName);
+			var failedTypes = results.InvalidTypes.Any(t => !ignoredTypes.Contains(t.ToSignature()));
+			var failedEvents = results.InvalidEvents.Any(e => !ignoreSet.Events.Select(t => t.FullName).Contains(e.ToSignature())
+				&& !ignoredTypes.Contains(e.DeclaringType.ToSignature()));
+			var failedFields = results.InvalidFields.Any(f => !ignoreSet.Fields.Select(t => t.FullName).Contains(f.ToSignature())
+				&& !ignoredTypes.Contains(f.DeclaringType.ToSignature()));
+			var failedMethods = results.InvalidMethods.Any(m => !ignoreSet.Methods.Select(t => t.FullName).Contains(m.ToSignature())
+				&& !ignoredTypes.Contains(m.DeclaringType.ToSignature()));
+			var failedProperties = results.InvalidProperties.Any(p => !ignoreSet.Properties.Select(t => t.FullName).Contains(p.ToSignature())
+				&& !ignoredTypes.Contains(p.DeclaringType.ToSignature()));
 
 			return failedTypes
 				|| failedEvents
